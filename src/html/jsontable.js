572c32fcca4efe8c30198f10b52a30b26aa49c00
Kaci Hernandez Kluesner <kacihernandezkluesner@Kacis-MacBook-Air.local>
2018-12-18 02:59:30 -0500

    new json stuff

---
+++ src/html/jsontable.js
@@ -0,0 +1,81 @@
+$(document).ready(function() {
+    $('#example').DataTable( {
+        "ajax": {"data/objects.txt",
+        "columns": [
+            { {"data": "name" },
+
+                {"data": "Latitude" }
+                {"data": "Longitude" }
+                {"data": "ParcelID" }
+                {"data": "AssessChange" }
+                {"data": "ApprChange" }
+                {"data": "OBJECTID" }
+                {"data": "TaxYear" }
+                {"data": "Address" }
+                {"data": "AddrNumber" }
+                {"data": "AddrPreDir" }
+                {"data": "AddrStreet" }
+                {"data": "AddrSuffix" }
+                {"data": "AddrPosDir" }
+                {"data": "AddrUntTyp" }
+                {"data": "AddrUnit" }
+                {"data": "Owner" }
+                {"data": "OwnerAddr1" }
+                {"data": "OwnerAddr2" }
+                {"data": "TaxDist" }
+                {"data": "TotAssess" }
+                {"data": "LandAssess" }
+                {"data": "ImprAssess" }
+                {"data": "TotAppr" }
+                {"data": "LandAppr" }
+                {"data": "ImprAppr" }
+                {"data": "LUCode" }
+                {"data": "ClassCode" }
+                {"data": "ExCode" }
+                {"data": "LivUnits" }
+                {"data": "LandAcres" }
+                {"data": "NbrHood" }
+                {"data": "Subdiv" }
+                {"data": "SubdivNum" }
+                {"data": "SubdivLot" }
+                {"data": "SubdivBlck" }
+                {"data": "FeatureID" }
+                {"data": "SHAPESTArea" }
+                {"data": "SHAPESTLength" }
+                {"data": "OBJECTID" }
+                {"data": "DIGEST" }
+                {"data": "SITUS" }
+                {"data": "TAXPIN" }
+                {"data": "ATRPIN" }
+                {"data": "TAX_DISTR" }
+                {"data": "OWNER1" }
+                {"data": "OWNER2" }
+                {"data": "ADD2" }
+                {"data": "ADD3" }
+                {"data": "ADD4" }
+                {"data": "ADD5" }
+                {"data": "LUC" }
+                {"data": "NBHD" }
+                {"data": "PROP_CLASS" }
+                {"data": "CLASS" }
+                {"data": "TOT_APPR" }
+                {"data": "TOT_ASSESS" }
+                {"data": "IMPR_APPR" }
+                {"data": "LAND_APPR" }
+                {"data": "FUL_EX_COD" }
+                {"data": "VAL_ACRES" }
+                {"data": "STRUCT_FLR" }
+                {"data": "STRUCT_YR" }
+                {"data": "TIEBACK" }
+                {"data": "TAXYEAR" }
+                {"data": "STATUS_COD" }
+                {"data": "LIV_UNITS" }
+                {"data": "PCODE" }
+                {"data": "UNIT_NUM" }
+                {"data": "GID" }
+                {"data": "EXTVER" }
+                {"data": "ShapeSTArea" }
+                {"data": "ShapeSTLength" }
+        ]
+    } );
+} );
